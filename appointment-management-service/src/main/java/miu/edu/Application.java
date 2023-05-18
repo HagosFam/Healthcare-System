97cff33fbd9f82ad1484b8c94026b42bcb5a6de5
Gebreegziabher Gebregiorgis <3g.mit02@gmail.com>
2023-05-17 21:43:58 -0500

    new files

---
+++ appointment-management-service/src/main/java/miu/edu/Application.java
@@ -0,0 +1,17 @@
+package miu.edu;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String[] args) throws Exception {
+        System.out.println("Microservice running");
+    }
+}
