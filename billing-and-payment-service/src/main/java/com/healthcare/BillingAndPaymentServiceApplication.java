b7143ee6b207e3904fa62a956e7b67338c70e49d
Gebreegziabher Gebregiorgis <3g.mit02@gmail.com>
2023-05-21 21:32:00 -0500

    changes on docker-compose files

--- billing-and-payment-service/src/main/java/com/healthcare/BillingAndPaymentServiceApplication.java
+++ billing-and-payment-service/src/main/java/com/healthcare/BillingAndPaymentServiceApplication.java
@@ -1,15 +1,14 @@
 package com.healthcare;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class BillingAndPaymentServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(BillingAndPaymentServiceApplication.class, args);
 	}
 
 	@Override
