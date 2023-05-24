c3644abf5f6376d5e7ea7c37f90414e5acdec76c
Gebreegziabher Gebregiorgis <3g.mit02@gmail.com>
2023-05-24 02:30:59 -0500

    add graceful message on unauthorized request

--- api-gateway/src/main/java/com/healthcare/apigateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/healthcare/apigateway/filter/AuthenticationFilter.java
@@ -1,11 +1,20 @@
 package com.healthcare.apigateway.filter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.healthcare.apigateway.util.JwtUtil;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config>{
@@ -23,9 +32,8 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
     public GatewayFilter apply(Config config) {
         return ((exchange, chain) -> {
             if (validator.isSecured.test(exchange.getRequest())) {
-                //header contains token or not
                 if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                    throw new RuntimeException("missing authorization header");
+                    return handleUnauthorizedAccess(exchange.getResponse(), "Missing authorization header");
                 }
 
                 String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
@@ -35,12 +43,31 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
                 try {
                     jwtUtil.validateToken(authHeader);
                 } catch (Exception e) {
-                    throw new RuntimeException("Unauthorized access");
+                    return handleUnauthorizedAccess(exchange.getResponse(), "Unauthorized access");
                 }
             }
             return chain.filter(exchange);
         });
     }
+
+    private Mono<Void> handleUnauthorizedAccess(ServerHttpResponse response, String errorMessage) {
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+        ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED, errorMessage);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        byte[] errorBytes;
+        try {
+            errorBytes = objectMapper.writeValueAsBytes(apiError);
+        } catch (JsonProcessingException e) {
+            errorBytes = errorMessage.getBytes(StandardCharsets.UTF_8);
+        }
+
+        DataBuffer buffer = response.bufferFactory().wrap(errorBytes);
+        return response.writeWith(Mono.just(buffer));
+    }
+
     public static class Config {
     }
 }
